1dbbba8c59fccc500f8d03a67fc086994e49adde
ngdangdat <ngdangdat09@gmail.com>
2023-10-15 02:58:36 +0700

    feat: apt-get to apt

--- utils.sh
+++ utils.sh
@@ -7,7 +7,7 @@ getPackageManager() {
     osInfo[/etc/arch-release]=pacman
     osInfo[/etc/gentoo-release]=emerge
     osInfo[/etc/SuSE-release]=zypp
-    osInfo[/etc/debian_version]=apt-get
+    osInfo[/etc/debian_version]=apt
     osInfo[/etc/alpine-release]=apk
     for f in ${!osInfo[@]}
     do
